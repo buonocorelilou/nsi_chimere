a7a238f7700821c033a84a3cdb01bbabca378bdf
buonocorelilou <buonocorelilou@gmail.com>
2025-03-02 20:50:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function foncEntre(){
-    document.querySelector("#maDiv").classList.remove("blanc");
-    document.querySelector("#maDiv").classList.add("rouge");
-}
-function foncQuitte() {
-    document.querySelector("#maDiv").classList.remove("rouge");
-    document.querySelector("#maDiv").classList.add("blanc");
+function validation_formulaire(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    var tete = document.getElementById('tete').value;
+    var corps = document.getElementById('corps').value;
+    var pattes = document.getElementById('pattes').value;
+
+    if (tete === "" || corps === "" || pattes === "") {
+        alert("Veuillez remplir tous les champs !");
+    } else {
+        var userInput = `Créé l'image d'une chimère avec une tête de ${tete}, un corps de ${corps}, et des pattes de ${pattes} dans un style réaliste.`;
+        document.getElementById('chimere').innerText = userInput;
+    }
 }
